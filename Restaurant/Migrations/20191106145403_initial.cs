e40e4aaf441129e24ac45198c0eefed877ead88c
Fernando Lopes <32465227+fernandojuniorlopes@users.noreply.github.com>
2019-11-06 14:58:47 +0000

    Added a new migration and updated databases

---
+++ Restaurant/Migrations/20191106145403_initial.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Restaurant.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "FoodMenu",
+                columns: table => new
+                {
+                    FoodMenuId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<int>(maxLength: 200, nullable: false),
+                    Description = table.Column<string>(nullable: false),
+                    Price = table.Column<decimal>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FoodMenu", x => x.FoodMenuId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FoodMenu");
+        }
+    }
+}
